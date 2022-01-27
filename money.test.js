ec2db4ab2ffa6131b33b3cbbf92ffb8a10f6e00f
rsgilbert <gssenyonjo@gmail.com>
2022-01-27 23:19:42 +0000

    Implemented MoneyBag

--- money.test.js
+++ money.test.js
@@ -2,11 +2,16 @@ const Money = require("./money");
 const assert = require('assert');
 
 describe('money', () => {
+    let m1;
+    let m2;
+    beforeEach(() => {
+        m1 = new Money(12, 'UGX');
+        m2 = new Money(50, 'UGX');
+    })
     test('simple add', () => {
         // Code which creates the objects we will interact
         // with during the test. Called a test fixture.
-        let m1 = new Money(12, 'UGX');
-        let m2 = new Money(50, 'UGX');
+     
         let expected = new Money(62, 'UGX');
         // Code which exercises the objects in the fixture.
         let result = m1.add(m2);
@@ -16,12 +21,10 @@ describe('money', () => {
     });
 
     test('equals', () => {
-        let m1 = new Money(5, 'KE');
-        let m2 = new Money(12, 'KE');
         expect(m1.equals(null)).toBe(false);
         expect(m1).toBe(m1);
-        expect(m2).toEqual(new Money(12, 'KE'));
+        expect(m2).toEqual(new Money(50, 'UGX'));
         assert(!m1.equals(m2));
-        assert(m1.equals(new Money(5, 'KE')));
+        assert(m1.equals(new Money(12, 'UGX')));
     });
 })
---
+++ moneybag.js
@@ -0,0 +1,31 @@
+const Money = require("./money");
+
+class MoneyBag {
+    /**
+     * @type Money[]
+     */
+    _monies = [];
+
+    /**
+     * 
+     * @param  {...Money} monies 
+     */
+    constructor(...monies) {
+        monies.forEach(appendMoney);
+    }
+
+    /**
+     * 
+     * @param {Money} money 
+     */
+    appendMoney(money) {
+        let foundMoney = this._monies.find(m => m.currency === money.currency);
+        if(foundMoney) {
+            foundMoney.add(money);
+        }
+        else {
+            this._monies.push(money);
+        }
+
+    }
+}
